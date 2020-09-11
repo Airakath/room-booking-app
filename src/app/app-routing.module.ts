3d5ee2de6019d976372385bb57f60fbcef5c7de7
nicolas <bernin69@gmail.com>
2020-09-11 10:30:04 +0200

    generate module and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule),
+    data: {
+
+    },
+  },
+  {
+    path: 'inscription',
+    loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule),
+    data: {
+
+    },
+  },
+  {
+    path: 'connexion',
+    loadChildren: () => import('./sign-in/sign-in.module').then(m => m.SignInModule),
+    data: {
+
+    },
+  },
+  {
+    path: '404NotFound',
+    loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule),
+    data: {
+
+    },
+  },
+  {
+    path: '**',
+    redirectTo: '/404NotFound',
+  },  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
